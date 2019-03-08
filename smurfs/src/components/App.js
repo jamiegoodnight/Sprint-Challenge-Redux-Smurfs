ce36e921159a91dd0769fd279f0ff06dd63c31d7
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-08 12:54:09 -0500

    Almost forgot to answer questions!

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -5,12 +5,6 @@ import './App.css';
 import { fetchSmurfsStart } from '../actions/index'
 import { smurfSmurf } from '../actions/index'
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
 class App extends Component {
   state={
     smurf: {
@@ -46,7 +40,7 @@ class App extends Component {
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
           {this.props.smurfs.map((x, i) => (
-            <div key={x.name}>
+            <div  className="smurfs" key={x.name}>
               <h4>{x.name}</h4>
               <p>{x.age}</p>
               <p>{x.height}</p>
