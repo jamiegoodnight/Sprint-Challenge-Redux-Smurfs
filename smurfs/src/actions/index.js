d2d6c97425bd60b8f0c2930fbba2c6652013432f
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-08 11:30:23 -0500

    Building out actions and app to retrieve smurfs. Moving on to rreducers.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+import axios from 'axios'
+
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
+
+export const fetchSmurfsStart = () => dispatch => {
+  dispatch ({type: FETCH_SMURFS_START});
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then(res => {
+    console.log(res)
+    dispatch({type: FETCH_SMURFS_SUCCESS})
+  })
+  .catch(err => {
+    console.log(err)
+    dispatch({type: FETCH_SMURFS_FAILURE, payload: "error"})
+  })
+}
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import './App.css';
+import { fetchSmurfsStart } from '../actions/index'
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +11,34 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  state={
+    smurf: {
+      name: '',
+      age: '',
+      height: '',
+    }
+  }
+  componentDidMount(){
+    this.props.fetchSmurfsStart()
+  }
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs
+})
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchSmurfsStart
+  }
+)(App);
